e480b0bb080a2ede055c18c2e3072c70271e2a3c
Joe Schofield <joeschofield@live.co.uk>
2017-09-01 14:23:10 +0100

    Use module to check cidr IP

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const ip = require('ip');
+const cidrv4 = require('cidr-regex').cidrv4;
 
 const errorMessage = 'IP supplied is not valid';
 
@@ -11,9 +12,8 @@ function convert(cidrIp, ip2) {
   If second IP address is supplied, ensure that both are IPv4 format
   */
   if (ip2) {
-    if (ip.isV4Format(cidrIp) && ip.isV4Format(ip2)) {
+    if (ip.isV4Format(cidrIp) && ip.isV4Format(ip2))
       return getRange(cidrIp, ip2);
-    }
 
     return errorMessage;
   }
@@ -21,10 +21,7 @@ function convert(cidrIp, ip2) {
   /*
   Ensure IP is valid and in CIDR format
   */
-  if (typeof cidrIp !== 'string' ||
-    !ip.isV4Format(cidrIp.split('/')[0]) ||
-    !cidrIp.split('/')[1] ||
-    cidrIp.split('/')[1] > 255)
+  if (!cidrv4.test(cidrIp))
     return errorMessage;
 
   const subnet = ip.cidrSubnet(cidrIp);
@@ -40,9 +37,8 @@ function getRange(ip1, ip2) {
   let firstAddressLong = ip.toLong(ip1);
   const lastAddressLong = ip.toLong(ip2);
 
-  for (firstAddressLong; firstAddressLong <= lastAddressLong; firstAddressLong++) {
+  for (firstAddressLong; firstAddressLong <= lastAddressLong; firstAddressLong++)
     ips.push(ip.fromLong(firstAddressLong));
-  }
 
   return ips;
 }
