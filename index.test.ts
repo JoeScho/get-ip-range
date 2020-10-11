85d49a05fc6dc27d2857e5cc26a5b129fc7d133b
JoeScho <joeschofield@live.co.uk>
2020-10-11 21:14:23 +0100

    use arrow fn

--- index.test.ts
+++ index.test.ts
@@ -18,107 +18,107 @@ const successResponsev6 = [
   '::ffff:102:307',
 ]
 
-describe('convert', function () {
-  describe('for cidr notation', function () {
-    it('should return an error if the value is supplied is null', function () {
+describe('convert', () => {
+  describe('for cidr notation', () => {
+    it('should return an error if the value is supplied is null', () => {
       const fn = () => getIPRange(null);
       expect(fn).toThrow();
     });
 
-    it('should return an error if the value is supplied is undefined', function () {
+    it('should return an error if the value is supplied is undefined', () => {
       const fn = () => getIPRange(undefined);
       expect(fn).toThrow();
     });
 
-    it('should return an error if the IP address supplied is invalid', function () {
+    it('should return an error if the IP address supplied is invalid', () => {
       const fn = () => getIPRange('abc');
       expect(fn).toThrow();
     });
 
-    it('should return an error if the IP address is not in CIDR notation', function () {
+    it('should return an error if the IP address is not in CIDR notation', () => {
       const fn = () => getIPRange('10.1.128.0');
       expect(fn).toThrow();
     });
 
-    it('should return an error if the IP address uses numbers which are too high', function () {
+    it('should return an error if the IP address uses numbers which are too high', () => {
       const fn = () => getIPRange('192.168.1.134/256');
       expect(fn).toThrow();
     });
 
-    it('should return an array of IP addresses within the specified range', function () {
+    it('should return an array of IP addresses within the specified range', () => {
       expect(getIPRange('192.168.1.134/29')).toEqual(successResponsev4);
     });
 
-    it('should support IPv6', function () {
+    it('should support IPv6', () => {
       expect(getIPRange('0:0:0:0:0:ffff:102:304/126')).toEqual(successResponsev6);
     });
   });
 });
 
-describe('for two IP addresses', function () {
-  it('should return an error if one of the IP addresses supplied is invalid', function () {
+describe('for two IP addresses', () => {
+  it('should return an error if one of the IP addresses supplied is invalid', () => {
     const fn = () => getIPRange('abc', '192.168.0.1');
     expect(fn).toThrow();
   });
 
-  it('should return an error if one of the IP addresses supplied is invalid', function () {
+  it('should return an error if one of the IP addresses supplied is invalid', () => {
     const fn = () => getIPRange('192.168.0.1', 'abc');
     expect(fn).toThrow();
   });
 
-  it('should return an error if one of the IP addresses is in CIDR notation', function () {
+  it('should return an error if one of the IP addresses is in CIDR notation', () => {
     const fn = () => getIPRange('10.1.128.0/29', '10.1.128.0');
     expect(fn).toThrow();
   });
 
-  it('should return an error if one of the IP addresses is in CIDR notation', function () {
+  it('should return an error if one of the IP addresses is in CIDR notation', () => {
     const fn = () => getIPRange('10.1.128.0', '10.1.128.0/29');
     expect(fn).toThrow();
   });
 
-  it('should return an error if one of the IP addresses is in null', function () {
+  it('should return an error if one of the IP addresses is in null', () => {
     const fn = () => getIPRange(null, '10.1.128.0');
     expect(fn).toThrow();
   });
 
-  it('should return an error if one of the IP addresses is in null', function () {
+  it('should return an error if one of the IP addresses is in null', () => {
     const fn = () => getIPRange('10.1.128.0', null);
     expect(fn).toThrow();
   });
 
-  it('should return an error if one of the IP addresses is in undefined', function () {
+  it('should return an error if one of the IP addresses is in undefined', () => {
     const fn = () => getIPRange(undefined, '10.1.128.0');
     expect(fn).toThrow();
   });
 
-  it('should return an error if one of the IP addresses is in undefined', function () {
+  it('should return an error if one of the IP addresses is in undefined', () => {
     const fn = () => getIPRange('10.1.128.0', undefined);
     expect(fn).toThrow();
   });
 
-  it('should return an error if one IP address has numbers which are too high', function () {
+  it('should return an error if one IP address has numbers which are too high', () => {
     const fn = () => getIPRange('192.168.1.134/256', '192.168.1.134');
     expect(fn).toThrow();
   });
 
-  it('should return an error if one IP address has numbers which are too high', function () {
+  it('should return an error if one IP address has numbers which are too high', () => {
     const fn = () => getIPRange('192.168.1.134', '192.168.1.134/256');
     expect(fn).toThrow();
   });
 
-  it('should return an array of IP addresses within the specified range', function () {
+  it('should return an array of IP addresses within the specified range', () => {
     expect(getIPRange('192.168.1.128', '192.168.1.135')).toEqual(successResponsev4);
   });
 
-  it('should support IPv6', function () {
+  it('should support IPv6', () => {
     expect(getIPRange('::ffff:102:304', '::ffff:102:307')).toEqual(successResponsev6);
   });
 
-  it('should support hyphenated range in IPv4', function () {
+  it('should support hyphenated range in IPv4', () => {
     expect(getIPRange('192.168.1.128-192.168.1.135')).toEqual(successResponsev4);
   });
 
-  it('should support hyphenated range in IPv5', function () {
+  it('should support hyphenated range in IPv5', () => {
     expect(getIPRange('::ffff:102:304-::ffff:102:307')).toEqual(successResponsev6);
   });
 });
